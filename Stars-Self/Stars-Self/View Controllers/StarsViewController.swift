05c7889a5be34f25c2cd8fc719134abfd51fbe3b
Percy <percival.ngan@gmail.com>
2019-12-26 12:32:35 -0600

    Connected the outlets & buttons to the class

--- Stars-Self/Stars-Self/View Controllers/StarsViewController.swift
+++ Stars-Self/Stars-Self/View Controllers/StarsViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class StarsViewController: UIViewController {
 
+	// MARK: - Outlets
+	@IBOutlet weak var nameTextField: UITextField!
+	@IBOutlet weak var distanceTextField: UITextField!
+
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+	}
+
+	// MARK: - Actions
+	@IBAction func printStarButton(_ sender: UIButton) {
+	}
+	@IBAction func createStarButton(_ sender: UIButton) {
 	}
 
 
