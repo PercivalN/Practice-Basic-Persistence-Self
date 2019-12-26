0296923bf8079fe3f0ee6476e9916fda7e4eb8ae
Percy <percival.ngan@gmail.com>
2019-12-26 12:37:57 -0600

    Added Model and Model Controller files

---
+++ Stars-Self/Stars-Self/Model Controller/StarController.swift
@@ -0,0 +1,9 @@
+//
+//  StarController.swift
+//  Stars-Self
+//
+//  Created by Percy Ngan on 12/26/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
---
+++ Stars-Self/Stars-Self/Models/Star.swift
@@ -0,0 +1,16 @@
+//
+//  Star.swift
+//  Stars-Self
+//
+//  Created by Percy Ngan on 12/26/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
+
+struct Star: Codable {
+	let name: String
+	let distance: Double
+
+
+}
